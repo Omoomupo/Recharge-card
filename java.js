bf3b94495c33c9b371aded32138712f6fb9ada0b
Omoomupo <59165872+Omoomupo@users.noreply.github.com>
2021-07-22 21:01:25 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,275 @@
+//MTN
+//  This function will multiply the quantity of Mtn100 by the price per unit.
+    var valueOfMtn100;
+    function multiplyTheValueOfMtn100ByTheQuantity(){
+        
+        var pricePerUnitOfMtn100 = document.getElementById("mtn100").value;
+        var quantityOfMtn100 = document.getElementById("num1").value;
+        
+        valueOfMtn100 = pricePerUnitOfMtn100 * quantityOfMtn100;
+       
+        document.getElementById("qty1").innerHTML = quantityOfMtn100;
+        document.getElementById("total1").innerHTML = valueOfMtn100;
+   }
+
+// This function will multiply the quantity of Mtn200 by the price per unit.
+    var valueOfMtn200;
+    function multiplyTheValueOfMtn200ByTheQuantity(){
+        
+        var pricePerUnitOfMtn200 = document.getElementById("mtn200").value;
+        var quantityOfMtn100 = document.getElementById("num2").value;
+        
+        valueOfMtn200 = pricePerUnitOfMtn200 * quantityOfMtn100;
+       
+        document.getElementById("qty2").innerHTML = quantityOfMtn100;
+        document.getElementById("total2").innerHTML = valueOfMtn200;
+   }
+
+// This function will multiply the quantity of Mtn500 by the price per unit.
+    var valueOfMtn500;
+    function multiplyTheValueOfMtn500ByTheQuantity(){
+        
+        var pricePerUnitOfMtn500 = document.getElementById("mtn500").value;
+        var quantityOfMtn100 = document.getElementById("num3").value;
+        
+        valueOfMtn500 = pricePerUnitOfMtn500 * quantityOfMtn100;
+       
+        document.getElementById("qty3").innerHTML = quantityOfMtn100;
+        document.getElementById("total3").innerHTML = valueOfMtn500;
+   }
+    
+// This function will multiply the quantity of Mtn1000 by the price per unit.
+    var valueOfMtn1000;
+    function multiplyTheValueOfMtn1000ByTheQuantity(){
+        
+        var pricePerUnitOfMtn1000 = document.getElementById("mtn1000").value;
+        var quantityOfMtn1000 = document.getElementById("num4").value;
+        
+        valueOfMtn1000 = pricePerUnitOfMtn1000 * quantityOfMtn1000;
+       
+        document.getElementById("qty4").innerHTML = quantityOfMtn1000;
+        document.getElementById("total4").innerHTML = valueOfMtn1000;
+
+   }
+//AIRTEL
+// This function will multiply the quantity of Airtel100 by the price per unit.
+    var valueOfAirtel100;
+    function multiplyTheValueOfAirtel100ByTheQuantity(){
+        
+        var pricePerUnitOfAirtel100 = document.getElementById("airtel100").value;
+        var quantityOfAirtel100 = document.getElementById("num5").value;
+        
+        valueOfAirtel100 = pricePerUnitOfAirtel100 * quantityOfAirtel100;
+       
+        document.getElementById("qty5").innerHTML = quantityOfAirtel100;
+        document.getElementById("total5").innerHTML = valueOfAirtel100;
+
+   }
+    
+// This function will multiply the quantity of Airtel200 by the price per unit.
+    var valueOfAirtel200;
+    function multiplyTheValueOfAirtel200ByTheQuantity(){
+        
+        var pricePerUnitOfAirtel200 = document.getElementById("airtel200").value;
+        var quantityOfAirtel200 = document.getElementById("num6").value;
+        
+        valueOfAirtel200 = pricePerUnitOfAirtel200 * quantityOfAirtel200;
+       
+        document.getElementById("qty6").innerHTML = quantityOfAirtel200;
+        document.getElementById("total6").innerHTML = valueOfAirtel200;
+
+   }
+    
+// This function will multiply the quantity of Airtel500 by the price per unit.
+    var valueOfAirtel500;
+    function multiplyTheValueOfAirtel500ByTheQuantity(){
+        
+        var pricePerUnitOfAirtel500 = document.getElementById("airtel500").value;
+        var quantityOfAirtel500 = document.getElementById("num7").value;
+        
+        valueOfAirtel500 = pricePerUnitOfAirtel500 * quantityOfAirtel500;
+       
+        document.getElementById("qty7").innerHTML = quantityOfAirtel500;
+        document.getElementById("total7").innerHTML = valueOfAirtel500;
+
+   }
+    
+// This function will multiply the quantity of Airtel1000 by the price per unit.
+    var valueOfAirtel1000
+    function multiplyTheValueOfAirtel1000ByTheQuantity(){
+        
+        var pricePerUnitOfAirtel1000 = document.getElementById("airtel1000").value;
+        var quantityOfAirtel1000 = document.getElementById("num8").value;
+        valueOfAirtel1000 = pricePerUnitOfAirtel1000 * quantityOfAirtel1000;
+       
+        document.getElementById("qty8").innerHTML = quantityOfAirtel1000;
+        document.getElementById("total8").innerHTML = valueOfAirtel1000;
+
+   }
+
+//GLO
+// This function will multiply the quantity of Glo100 by the price per unit.
+    var valueOfGlo100;
+    function multiplyTheValueOfGlo100ByTheQuantity(){
+        
+        var pricePerUnitOfGlo100 = document.getElementById("glo100").value;
+        var quantityOfGlo100 = document.getElementById("num9").value;
+        
+        valueOfGlo100 = pricePerUnitOfGlo100 * quantityOfGlo100;
+       
+        document.getElementById("qty9").innerHTML = quantityOfGlo100;
+        document.getElementById("total9").innerHTML = valueOfGlo100;
+
+   }
+// This function will multiply the quantity of Glo200 by the price per unit.
+    var valueOfGlo200;
+    function multiplyTheValueOfGl200ByTheQuantity(){
+        
+        var pricePerUnitOfGlo200 = document.getElementById("glo200").value;
+        var quantityOfGlo200 = document.getElementById("num10").value;
+        
+        valueOfGlo200 = pricePerUnitOfGlo200 * quantityOfGlo200;
+       
+        document.getElementById("qty10").innerHTML = quantityOfGlo200;
+        document.getElementById("total10").innerHTML = valueOfGlo200;
+
+   }
+// This function will multiply the quantity of Glo500 by the price per unit.
+    var valueOfGlo500
+    function multiplyTheValueOfGlo500ByTheQuantity(){
+        
+        var pricePerUnitOfGlo500 = document.getElementById("glo500").value;
+        var quantityOfGlo500 = document.getElementById("num11").value;
+        
+        valueOfGlo500 = pricePerUnitOfGlo500 * quantityOfGlo500;
+       
+        document.getElementById("qty11").innerHTML = quantityOfGlo500;
+        document.getElementById("total11").innerHTML = valueOfGlo500;
+
+   }
+// This function will multiply the quantity of Glo1000 by the price per unit.
+    var valueOfGlo1000;
+    function multiplyTheValueOfGlo1000ByTheQuantity(){
+        
+        var pricePerUnitOfGlo1000 = document.getElementById("glo1000").value;
+        var quantityOfGlo1000 = document.getElementById("num12").value;
+        
+        valueOfGlo1000 = pricePerUnitOfGlo1000 * quantityOfGlo1000;
+       
+        document.getElementById("qty12").innerHTML = quantityOfGlo1000;
+        document.getElementById("total12").innerHTML = valueOfGlo1000;
+
+   }
+//9MOBILE
+// This function will multiply the quantity of 9mobile100 by the price per unit.
+    
+    var valueOfMobile100;
+    function multiplyTheValueOfMobile100ByTheQuantity(){
+        var pricePerUnitOfMobile100 = document.getElementById("9mobile100").value;
+        var quantityOfMobile100 = document.getElementById("num13").value;
+        valueOfMobile100 = pricePerUnitOfMobile100 * quantityOfMobile100;
+       
+        document.getElementById("qty13").innerHTML = quantityOfMobile100;
+        document.getElementById("total13").innerHTML = valueOfMobile100;
+
+   }
+
+// This function will multiply the quantity of 9mobile200 by the price per unit.
+    var valueOfMobile200;
+    function multiplyTheValueOfMobile200ByTheQuantity(){
+        var pricePerUnitOfMobile200 = document.getElementById("9mobile200").value;
+        var quantityOfMobile200 = document.getElementById("num14").value;
+        valueOfMobile200 = pricePerUnitOfMobile200 * quantityOfMobile200;
+       
+        document.getElementById("qty14").innerHTML = quantityOfMobile200;
+        document.getElementById("total14").innerHTML = valueOfMobile200;
+
+   }
+    
+// This function will multiply the quantity of 9mobile500 by the price per unit.
+    var valueOfMobile500;
+    function multiplyTheValueOfMobile500ByTheQuantity(){
+        var pricePerUnitOfMobile500 = document.getElementById("9mobile500").value;
+        var quantityOfMobile500 = document.getElementById("num15").value;
+        valueOfMobile500 = pricePerUnitOfMobile500 * quantityOfMobile500;
+       
+        document.getElementById("qty15").innerHTML = quantityOfMobile500;
+        document.getElementById("total15").innerHTML = valueOfMobile500;
+
+   }
+    
+    // This function will multiply the quantity of 9mobile1000 by the price per unit.
+    var valueOfMobile1000;
+    function multiplyTheValueOfMobile1000ByTheQuantity(){
+        var pricePerUnitOfMobile1000 = document.getElementById("9mobile1000").value;
+        var quantityOfMobile1000 = document.getElementById("num16").value;
+        valueOfMobile1000 = pricePerUnitOfMobile1000 * quantityOfMobile1000;
+       
+        document.getElementById("qty16").innerHTML = quantityOfMobile1000;
+        document.getElementById("total16").innerHTML = valueOfMobile1000;
+        
+   }
+   
+
+//    Calculate All
+    var totalAmount;
+    function calculateAll(){
+       
+        totalAmount = valueOfMtn100 + valueOfMtn200  + valueOfMtn500 + valueOfMtn1000 + valueOfAirtel100 + valueOfAirtel200 + valueOfAirtel500 + valueOfAirtel1000 + valueOfGlo100 + valueOfGlo200 + valueOfGlo500 + valueOfGlo1000 + valueOfMobile100 + valueOfMobile200 + valueOfMobile500 + valueOfMobile1000;
+        
+        document.getElementById("total").innerHTML = totalAmount;
+        
+    }
+
+
+function payment(){
+    document.getElementById("payment").innerHTML = "<h2  >You are to pay <h2> " + "₦" + totalAmount;
+}
+    
+//Print This print the stock receipts.
+function codespeedy(){
+var print_div = document.getElementById("table");
+var print_area = window.open();
+print_area.document.write(print_div.innerHTML);
+print_area.document.write('<link rel="stylesheet" href="style.css">');
+print_area.document.close();
+print_area.focus();
+print_area.print();
+print_area.close();
+		}
+
+
+//TIME AND DAY
+var clock = document.getElementById('time');
+var dates = document.getElementById('date');
+
+function time(){
+    var currentTime = new Date();
+ 
+    
+    var hours = currentTime.getHours() ;
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    
+    
+    var day = currentTime.getDate();
+    var month = 1 + currentTime.getMonth();
+    var year = currentTime.getFullYear();
+    var days = currentTime.getDay();
+    
+    
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+
+//    clock.textContent = hours  + ":" + minutes + ":" + seconds ;
+    dates.textContent = day + " / " + month + " / " + year ;
+}
+
+
+setInterval(time,1000);
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+
+}
